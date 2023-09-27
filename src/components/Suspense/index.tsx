4929ed86058aa94d97e44d283964be505ff16945
Maksim Malashkov <max.malaschkov@mail.ru>
2023-09-27 11:55:30 +0300

    fix: check error

--- src/components/Suspense/index.tsx
+++ src/components/Suspense/index.tsx
@@ -5,10 +5,12 @@ import Loading from '@components/Loading';
 import { SuspenseProps } from './interfaces';
 
 const getSuspenseDisplayName = (Component: React.FC): string => {
+  console.log(Component.displayName || Component.name || 'Component');
   return Component.displayName || Component.name || 'Component';
 };
 
 export const Suspense: React.FC<SuspenseProps> = ({ component: Component }: SuspenseProps) => {
+  console.log(getSuspenseDisplayName(Component));
   const LazyComponent = lazy(() => import(`@pages/${getSuspenseDisplayName(Component)}`));
 
   return (
